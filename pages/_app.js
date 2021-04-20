70b19050996dfea619bfa949de60af31c94b0735
Ritul Patel <49111661+codeonec@users.noreply.github.com>
2021-04-20 17:05:53 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -46,7 +46,6 @@ function MyApp({ Component, pageProps }) {
           <meta property="twitter:description" content="Library of top rated curated design resources for developers. UI Inspirations, UI Resources, Icons, Fonts, Illustrations and much more..."></meta>
           <meta property="twitter:image" content="https://designbase.vercel.app/og_tw_img.png"></meta>
           
-          <!-- Global site tag (gtag.js) - Google Analytics -->
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-H33LWYM277"></script>
           <script>
             window.dataLayer = window.dataLayer || [];
